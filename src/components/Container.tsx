74bf070b7d682e5b426045ef9354710d5dc9fb6f
vadistic <vadistic@gmail.com>
2018-05-30 16:27:06 +0200

    Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,22 @@
-import styled from 'react-emotion'
+import * as React from 'react'
+import * as cx from 'classnames'
 
-export const Container = styled.div`
-  ${tw('container mx-auto ')};
-`
+import { css } from 'react-emotion'
+
+interface BaseProps {
+  className?: string
+}
+
+interface ContainerProps extends BaseProps {
+  fluid?: boolean
+}
+
+export const Container: React.SFC<ContainerProps> = ({
+  children,
+  fluid,
+  className,
+}) => (
+  <div className={cx(className, !fluid && 'container', css(tw('mx-auto')))}>
+    {children}
+  </div>
+)
