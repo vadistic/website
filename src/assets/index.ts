b4b0f3a9697b6fc28d770736826cf389a8e3de4c
Jakub Wadas <vadistic@gmail.com>
2019-02-06 03:55:44 +0100

    fix(assets): added ids to illustrations

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,4 @@ import * as Icons from './icons'
 import * as Illustrations from './illustrations'
 import * as Logos from './logos'
 
-export { ReactComponent as Pattern } from './pattern.svg'
-
 export { Logos, Illustrations, Icons }
